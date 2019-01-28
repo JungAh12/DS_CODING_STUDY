2a02b26a03409dd0890d7dc38b72cd35aef6989b
Inseo Song <41552919+fortunetiger@users.noreply.github.com>
2019-01-28 12:04:32 +0900

    Create 2775.cpp

---
+++ InseoSong/Baekjoon/2775.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int count(int k, int n) {
+
+	int ans = 0;
+
+	if (k == 0) {
+		return n;
+	}
+	else {
+		for (int i = n-1; i > 0; i++) {
+			ans += count(k-1, i);
+		}
+	}
+	return ans;
+}
+
+int main() {
+
+	int t = 0, n = 0, k = 0;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> k;
+		cin >> n;
+
+		cout << count(k, n) << endl;
+	}
+
+	return 0;
+}
