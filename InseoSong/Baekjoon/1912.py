5132a779524ee361ebcbaee1a6224bd71b9f84e1
Inseo Song <41552919+fortunetiger@users.noreply.github.com>
2019-02-09 23:27:50 +0900

    Update 1912.py

--- InseoSong/Baekjoon/1912.py
+++ InseoSong/Baekjoon/1912.py
@@ -1,37 +1,34 @@
-n = int(input())
-s = input()
-num = list(map(int, s.split(' ')))
-
-cm = -2000
-cs = -2000
-m = []
-a = []
-for i in range(n):
-    if cs == -2000:
-        cs = num[i]
-        m.append(i)
-
-        if cs > cm:
-            cm = cs
-    else:
-        if cs > cs + num[i]:
-            if cs > cm:
-                cm = cs
-                a = m
-            cs = -2000
-            m = []
+def find(num):
+    p=[]
+    k=0
+    r_max = 0
+    l_max = 0
+    for n in num:
+        if k*n < 0:
+            p.append(k)
+            k=n
         else:
-            cs += num[i]
-            m.append(i)
+            k+=n
+    p.append(k)
 
-cm = 0
-print(a)
-if len(a) == 0:
-    cm = max(num)
-else:
-    for i in a:
-        if num[i] < 0:
-            continue
-        cm += num[i]
+    i = p.index(max(p))
 
-print(cm)
+    if p[i]<=0 :
+        return max(num)
+    else:
+        s = 0
+        if p[i]!=p[-1]:
+            for r in p[i+1:]:
+                s += r
+                if s>r_max:
+                    r_max = s
+        elif p[i]!=p[0]:
+            for l in p[i-1::-1]:
+                s += l
+                if s>l_max:
+                    l_max = s
+
+    return l_max + max(p) + r_max
+
+n= int(input())
+print(find(list(map(int, input().split(' ')[0:n]))))
